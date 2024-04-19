e451a934f2a664c43b32b5ccd0ec9ca7bcac91d8
Polar <rosyrdas@gmail.com>
2024-04-19 16:21:25 +0530

    length validation for password in login

--- server/src/frameworks/schemaValidators/login.ts
+++ server/src/frameworks/schemaValidators/login.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const LoginSchema = z.object({
     email: z.string({ required_error: "Email is required", invalid_type_error: "Email must be a string" }).email({ message: "Invalid email" }),
-    password: z.string({ required_error: "Password is required", invalid_type_error: "Password must be a string" }),
+    password: z.string({ required_error: "Password is required", invalid_type_error: "Password must be a string" }).min(8, { message: "Password must be minimum 8 characters" }),
 }).strict();
