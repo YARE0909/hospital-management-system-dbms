6812ddd59cb443661ae81993a073bc8ce796106e
Polar <rosyrdas@gmail.com>
2024-04-19 16:19:37 +0530

    fix: add only doctor login

--- server/src/routes/auth/login/controller.ts
+++ server/src/routes/auth/login/controller.ts
@@ -3,49 +3,31 @@ import { type Request, type Response } from "express";
 import { ZodError } from "zod";
 import jwt from "jsonwebtoken";
 import { fromZodError, isValidationError } from "zod-validation-error";
-import { Patients, Doctors } from "#src/entities/index.js";
+import { Doctors } from "#src/entities/index.js";
 import { SchemaValidators } from "#src/frameworks/index.js";
 
 export default async function login(req: Request, res: Response): Promise<ServerResponse> {
     const response: ServerResponse = { hasError: true, message: "An error occured while getting patients", data: null }
 
     try {
-        const { patientEmail, doctorEmail, password } = SchemaValidators.LoginSchema.parse(req.body);
-        if (patientEmail) {
-            const patientInfo = await Patients.getPatientByEmail(patientEmail);
-            if (!patientInfo) {
-                response.message = "Patient not found";
-                return response;
-            }
+        const { email, password } = SchemaValidators.LoginSchema.parse(req.body);
 
-            if (patientInfo.password !== password) {
-                response.message = "Invalid password";
-                return response;
-            }
-
-            const token = jwt.sign({ userId: patientInfo.id }, process.env.JWT_SECRET);
-
-            response.hasError = false;
-            response.message = "Patient fetched successfully";
-            response.data = { patientToken: token };
-        } else if (doctorEmail) {
-            const doctorInfo = await Doctors.getDoctorByEmail(doctorEmail);
-            if (!doctorInfo) {
-                response.message = "Doctor not found";
-                return response;
-            }
-
-            if (doctorInfo.password !== password) {
-                response.message = "Invalid password";
-                return response;
-            }
-
-            const token = jwt.sign({ userId: doctorInfo.id }, process.env.JWT_SECRET);
-
-            response.hasError = false;
-            response.message = "Doctor fetched successfully";
-            response.data = { doctorToken: token };
+        const doctorInfo = await Doctors.getDoctorByEmail(email);
+        if (!doctorInfo) {
+            response.message = "Doctor not found";
+            return response;
         }
+
+        if (doctorInfo.password !== password) {
+            response.message = "Invalid password";
+            return response;
+        }
+
+        const token = jwt.sign({ userId: doctorInfo.id }, process.env.JWT_SECRET);
+
+        response.hasError = false;
+        response.message = "Doctor fetched successfully";
+        response.data = { token };
 
         return response;
     } catch (err) {
